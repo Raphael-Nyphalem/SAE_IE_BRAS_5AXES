329ecca03ebb67bdf26cb700d7c5e748b028887b
Raphael-Nyphalem <nyphalem@pm.me>
2023-10-11 16:56:29 +0200

    clean

--- SAE/src/main.cpp
+++ SAE/src/main.cpp
@@ -6,10 +6,14 @@ const uint8_t ADDRESS_PSA = 0x7F;
 
 const uint8_t R_MODE_1= 0x00;
 const uint8_t R_PRE_SCALE = 0xFE;
+const uint8_t R_LED0_ON_L = 0x06;
+
 
 
 const uint8_t DEFAULT_MODE1 = 0x31;  
 const uint8_t PRESCALER = 25000000/4096/50;
+
+
 
 
 /**
@@ -27,7 +31,7 @@ void initialisation();
 void ecrire_sur_un_registre(uint8_t registre, uint8_t data);
 
 /***/
-void ecrire_sur_4_registre(uint8_t registre_n, uint8_t data[4]);
+void ecrire_sur_4_registre(uint8_t registre_ledn_ON_L, uint8_t data[4]);
 
 void setup() {
   Wire.begin();
@@ -38,7 +42,7 @@ void setup() {
   data[1] =0X00;
   data[2] =0XFF;
   data[3] =0X07;
-  ecrire_sur_4_registre(0x06,data);
+  ecrire_sur_4_registre(R_LED0_ON_L,data);
 }
 
 void loop() {
@@ -58,7 +62,7 @@ void ecrire_sur_un_registre(uint8_t registre, uint8_t data){
   uint8_t error =  Wire.endTransmission();
 }
 
-void ecrire_sur_4_registre(uint8_t registre_n,uint8_t data[4])
+void ecrire_sur_4_registre(uint8_t registre_ledn_ON_L,uint8_t data[4])
 {
   uint8_t data_ON_L = data[0];
   uint8_t data_ON_H = data[1] & 0x1F;
@@ -66,7 +70,7 @@ void ecrire_sur_4_registre(uint8_t registre_n,uint8_t data[4])
   uint8_t data_OFF_H= data[3]& 0x1F;
 
   Wire.beginTransmission(ADDRESS_PSA);
-  Wire.write(registre_n);
+  Wire.write(registre_ledn_ON_L);
   Wire.write(data_ON_L);
   Wire.write(data_ON_H);
   Wire.write(data_OFF_L);
