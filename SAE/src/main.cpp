f244b3f332261b7d69731e8deeddb676ea17c00f
Raphael-Nyphalem <nyphalem@pm.me>
2023-10-11 16:17:47 +0200

    test fonction

--- SAE/src/main.cpp
+++ SAE/src/main.cpp
@@ -12,19 +12,33 @@ const uint8_t DEFAULT_MODE1 = 0x11;
 const uint8_t PRESCALER = 25000000/(4096*50);
 
 
-
+/**
+ * @brief 
+ * 
+ */
 void initialisation();
+
+/**
+ * @brief 
+ * 
+ * @param registre 
+ * @param data 
+ */
 void ecrire_sur_un_registre(uint8_t registre, uint8_t data);
-void ecrire_sur_4_registre(uint8_t registre_n, 
-                        uint8_t data_ON_L,
-                        uint8_t data_ON_H,
-                        uint8_t data_OFF_L,
-                        uint8_t data_OFF_H
-                        );
+
+/***/
+void ecrire_sur_4_registre(uint8_t registre_n, uint8_t data[4]);
 
 void setup() {
   Wire.begin();
   initialisation();
+
+  uint8_t data[4];
+  data[0] =0X00;
+  data[1] =0X00;
+  data[2] =0XFF;
+  data[3] =0X07;
+  ecrire_sur_4_registre(0x06,data);
 }
 
 void loop() {
@@ -45,8 +59,14 @@ void ecrire_sur_un_registre(uint8_t registre, uint8_t data){
   uint8_t error =  Wire.endTransmission();
 }
 
-void ecrire_sur_4_registre(uint8_t registre_n,uint8_t data_ON_L,uint8_t data_ON_H,uint8_t data_OFF_L,uint8_t data_OFF_H)
+void ecrire_sur_4_registre(uint8_t registre_n,uint8_t data[4])
 {
+  uint8_t data_ON_L = data[0];
+  uint8_t data_ON_H = data[1] & 0x1F;
+  uint8_t data_OFF_L= data[2];
+  uint8_t data_OFF_H= data[3]& 0x1F;
+
+
   Wire.beginTransmission(ADDRESS_PSA);
   Wire.write(R_MODE_1);
   Wire.write(DEFAULT_MODE1 | 0x20);
